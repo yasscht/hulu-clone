b587b9a7b7de88a30abf763f3e7cdfeffe7d4ba8
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-19 17:43:43 +0000

    navbar implementation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import "./Nav.css";
+const navItems = [
+  { name: "Trending" },
+  { name: "Top Rated" },
+  { name: "Action" },
+  { name: "Comedy" },
+  { name: "Horror" },
+  { name: "Romance" },
+  { name: "Sci-fi" },
+  { name: "Western" },
+  { name: "Animation" },
+  { name: "Movie" },
+];
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="nav">
+      {navItems.map((item, index) => (
+        <h2 key={index}>{item.name}</h2>
+      ))}
+    </div>
+  );
 };
 
 export default Nav;
